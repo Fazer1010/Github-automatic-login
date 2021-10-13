8ac648a1012a213809c68037d8a3ce20936d3444
Fazer1010 <44116144+Fazer1010@users.noreply.github.com>
2021-10-14 01:14:32 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+# we can import Chromium webdriver and get those in the as the browser
 
 browser = webdriver.Firefox()
 username=""
